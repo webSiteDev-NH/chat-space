5c7720860fb629c93e21d98c29d9d358742af3bd
hiroki-n-8 <nonomurahiroki0807@gmail.com>
2020-01-08 21:26:01 +0900

    create

--- app/assets/javascripts/messege.js
+++ app/assets/javascripts/messege.js
@@ -1,8 +1,9 @@
 $(function(){
   function buildHTML(message){
-    if ( message.image ){
-      var html =
-        `<div class = message__list>
+    var image = ( message.image ) ? `<img class= "message__list__lower__image" src=${message.image} >` : "";
+
+    var html = `<div class="message__list", data-message-id="${message.id}">
+        <div class = message__list>
           <div class ="message__list__upper">
             <div class ="message__list__upper__sender">
               ${message.user_name}
@@ -15,29 +16,10 @@ $(function(){
             <p class="message__list__lower__text">
               ${message.text}
             </p>
+            ${image}
           </div>
-          <img src=${message.image} >
         </div>`
-      return html;
-    } else {
-      var html =
-      `<div class = message__list>
-        <div class ="message__list__upper">
-          <div class ="message__list__upper__sender">
-            ${message.user_name}
-          </div>
-          <div class="message__list__upper__date">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="message__list__lower">
-          <p class="message__list__lower__text">
-            ${message.text}
-          </p>
-        </div>
-      </div>`
-      return html;
-    };
+    return html;
   }
   $('#new_message').on('submit', function(e){
     e.preventDefault()
@@ -59,7 +41,35 @@ $(function(){
      })
      .fail(function() {
        alert("メッセージ送信に失敗しました");
-     });
+     })
      return false;
   })
+
+  var reloadMessages = function() {
+    last_message_id = $('.message__list:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $("#new_message")[0].reset();
+      $("block__input__box__send-btn").prop("disabled", false);
+      }
+    })
+    .fail(function(){
+      aleat('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+  }
 });
